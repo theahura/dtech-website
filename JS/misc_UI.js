aa07592dfe86d4e18b83eb84cdcf38246807381e
Amol Kapoor <theahura@gmail.com>
2015-05-15 03:39:52 -0400

    Added mumblr js button functionality, fixed broken link
    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- JS/misc_UI.js
+++ JS/misc_UI.js
@@ -1,10 +1,7 @@
   /* Scrollto for People */
     $('.down').click(function(){ $('html,body').animate({ scrollTop:   $('.gen-content').offset().top},300);});
+    $('#mumblr').click(function(){ $('html,body').animate({ scrollTop:   $('#Mumblr_Content').offset().top},300);});
     $('#defendthelounge').click(function(){ $('html,body').animate({ scrollTop:   $('#Defend_Lounge').offset().top},300);});
     $('#rumblr').click(function(){ $('html,body').animate({ scrollTop:   $('#Rumblr_Title').offset().top},300);});
     $('#ptable').click(function(){ $('html,body').animate({ scrollTop:   $('#P_Table').offset().top},300);});
-    $('#eric').click(function(){ $('html,body').animate({ scrollTop:   $('#Eric_Klawitter').offset().top},300);});
-    $('#derek').click(function(){ $('html,body').animate({ scrollTop:   $('#Derek_Hong').offset().top},300);});
-    $('#tracey').click(function(){ $('html,body').animate({ scrollTop:   $('#Tracey_Lin').offset().top},300);});
-    $('#kathleen').click(function(){ $('html,body').animate({ scrollTop:   $('#Kathleen_Zhu').offset().top},300);});
-    $('#alex').click(function(){ $('html,body').animate({ scrollTop:   $('#Alex_Epifano').offset().top},300);});
+    
