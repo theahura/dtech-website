323ed843db7a7e73dbdb5fdd630cb37d851f97f0
DEREK HONG <hong.derek.j@gmail.com>
2015-09-19 21:37:19 -0400

    updated the css on my end

---
+++ JS/projects_UI.js
@@ -0,0 +1,8 @@
+  /* Scrollto for People */
+    $('.down').click(function(){ $('html,body').animate({ scrollTop:   $('.gen-content').offset().top},300);});
+    $('#mumblr').click(function(){ $('html,body').animate({ scrollTop:   $('#Mumblr_Content').offset().top},300);});
+    $('#defendthelounge').click(function(){ $('html,body').animate({ scrollTop:   $('#Defend_Lounge').offset().top},300);});
+    $('#rumblr').click(function(){ $('html,body').animate({ scrollTop:   $('#Rumblr_Title').offset().top},300);});
+    $('#ptable').click(function(){ $('html,body').animate({ scrollTop:   $('#P_Table').offset().top},300);});
+    $('#rerep').click(function(){ $('html,body').animate({ scrollTop:   $('#ReRep-desc').offset().top},300);});
+    
