8b3b448e3063fcd17582a92fbedcb8075d619761
Amol Kapoor <theahura@gmail.com>
2015-09-22 19:50:08 -0400

    Signed-off-by: Amol Kapoor <theahura@gmail.com>

---
+++ JS/header.js
@@ -0,0 +1,29 @@
+var currentPage = "";
+
+window.onload = function() {
+	$('.to-home').click(function() {
+		window.location = "index.html";
+	});
+
+	$('.to-about').click(function() {
+		$('.content').fadeOut(function() {
+			window.location = "team.html";
+		});
+	});
+
+	$('.to-chalk').click(function() {
+		$('.content').fadeOut(function() {
+			window.location = "chalk.html";
+		});
+	});
+
+	$('.to-projects').click(function() {
+		$('.content').fadeOut(function() {
+			window.location = "projects.html";
+		});
+	});
+
+
+	if(!currentPage || currentPage !== 'chalk')
+		$('.content').fadeIn();
+}
